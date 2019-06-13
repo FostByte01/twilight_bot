8a46fc5f901721da6b2c42ba3f96fb4455f61d44
FrostByte01 <bjgrigor@gmail.com>
2019-06-13 13:14:14 -0500

    Mod commands now require appropriate permissions

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from asyncio import sleep
 
 
 class Moderation(commands.Cog):
@@ -6,10 +7,18 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.has_permissions(manage_messages=True)
     @commands.command(aliases=['purge', 'remove'], pass_context=True)
     async def clear(self, ctx, count: int):
         """Clear the specified amount of messages"""
         await ctx.channel.purge(limit=count+1)
+
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You do not have the permissions to do that!")
+            await sleep(2)
+            await ctx.channel.purge(limit=2)
 
 
 def setup(bot):
